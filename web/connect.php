3e407b2c36029e9d6445fbbe5c6cb46bf5e7bcbe
Majid AbdulAlrazzaq <31021135+iraqicoder@users.noreply.github.com>
2017-10-29 01:32:42 +0300

    Update connect.php

--- web/connect.php
+++ web/connect.php
@@ -2,7 +2,7 @@
 
 try{
 
-$handler = new PDO('mysql:host=127.0.0.1;dbname=ard','root','zozo1998');
+$handler = new PDO('mysql:host=127.0.0.1;dbname=ard','root','12345678');
 $handler->exec("SET NAMES 'utf8'");
 $handler->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
